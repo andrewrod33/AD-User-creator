235d1d81da55074f6e78e64c893048c21b727a5c
foxwill <williamfox+github@gmail.com>
2014-12-09 10:51:57 -0800

    cleanup
    code cleanup, adding notes

--- user_creator.py
+++ user_creator.py
@@ -6,7 +6,9 @@ Lets create some fake names.  We need to make sure we have
 Name, samAccountName, Description, Department, EmployeeID, Path, and Enabled
 """
 
-"""Values to generate random names"""
+"""
+Values to generate random names
+"""
 fname = ["Bob","John","Tom","Jack","Jill","Sam","Bill","Jerry","Frank","Thomas","Lucas"]
 lname = ["Smith","Jones","Davis","Fox","Chang","Washington","Adams","Jefferson","Skywalker","Palpatine"]
 samAname = 0
@@ -19,13 +21,14 @@ output = []
 
 
 
-"""dsadd command:
+"""
+dsadd command:
 dsadd user CN=John,CN=Users,DC=it,DC=uk,DC=savilltech,DC=com -samid John -pwd Pa55word 
 -fn John -ln Savill -display "John Savill" -email john@savilltech.com -webpg http://www.savilltech.com -pwdneverexpires yes 
 -memberof "CN=Domain Admins,CN=Users,DC=it,DC=uk,DC=savilltech,DC=com"
-
-CN=ADUser One,OU=Support,DC=pfoxcorp,DC=local
 """
+
+
 for x in range(500):
 	first = random.choice(fname)
 	last = random.choice(lname)
@@ -37,11 +40,6 @@ for x in range(500):
 	emp = str(employeeID)
 	employeeID += 10
 	pth = random.choice(path)
-	#record = "'" + name + "','" + samName + "','" + desc + "','" + depa + "','" + emp + "','" + pth + "','" + enabled + "'"
-	"""record = 'dsadd user CN="' + name + '",' + pth + ' -samid ' + samName + ' -upn ' + samName+ '@pfoxcorp.local' +' -empid '\
-	+ str(employeeID) +' -pwd P@ssw0rd2@ -fn ' + first + ' -ln ' + last + ' -display ' +\
-	'"' + name + '"' + ' -email ' + samName+str(samAname) + '@doesn0tex1st.com' + \
-	' -pwdneverexpires yes -memberof ' + pth + ' -disabled no' """
 	record = 'dsadd user CN="' + samName + str(samAname) + ',' + pth + '" -upn ' + samName+ '@pfoxcorp.local' +' -empid '\
 	+ str(employeeID) +' -pwd P@ssw0rd2@ -fn ' + first + ' -ln ' + last + ' -display ' +\
 	'"' + name + '"' + ' -email ' + samName+str(samAname) + '@doesn0tex1st.com' + \
@@ -49,13 +47,14 @@ for x in range(500):
 	if record not in output:
 		output.append(record)
 
-#for each in output:
-#	print each
+"""Lets remove any existing files so we do not write the same user record twice"""
+
 try:
 	os.remove('output.txt')
 except:
 	pass
 
+""" Write the output to output.txt"""
 f = open('output.txt', 'w')
 for each in output:
 	f.write(each + "\n")
